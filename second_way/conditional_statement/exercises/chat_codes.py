ed3b0ba6d130b2309caaae1f08ba55ac176f6d8f
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-05-13 09:52:33 +0200

    chat codes

---
+++ second_way/conditional_statement/exercises/chat_codes.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+for _ in range(n):
+    number = int(input())
+    if number == 88:
+        print("Hello")
+
+    elif number == 86:
+        print("How are you?")
+
+    elif number < 88:
+        print("GREAT!")
+
+    else:
+        print("Bye.")
