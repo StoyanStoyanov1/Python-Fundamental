358030d1be066e13a2abff868584533a6b149586
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-05-17 09:03:10 +0200

    integer operations

---
+++ second_way/data_types_and_variables/exercises/integer_operations.py
@@ -0,0 +1,8 @@
+first_number = int(input())
+second_number = int(input())
+three_number = int(input())
+fourth_number = int(input())
+
+result = (first_number + second_number) // three_number * fourth_number
+
+print(result)
