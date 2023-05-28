bc12d408671699c1c2776a73e4c37887a74f645b
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-05-28 06:47:48 +0200

    zoo

---
+++ second_way/classes_and_objects/lab/zoo.py
@@ -0,0 +1,36 @@
+class Zoo:
+    __animals = 0
+
+    def __init__(self, name):
+        self.name = name
+        self.mammals = []
+        self.fishes = []
+        self.birds = []
+
+    def add_animal(self, species, name):
+        if species == "mammal":
+            self.mammals.append(name)
+        elif species == "fish":
+            self.fishes.append(name)
+        elif species == "bird":
+            self.birds.append(name)
+
+        Zoo.__animals += 1
+
+    def get_info(self, species):
+        if species == "mammal":
+            return f"Mammals in {self.name}: {', '.join(self.mammals)}\nTotal animals: {Zoo.__animals}"
+        elif species == "fish":
+            return f"Fishes in {self.name}: {', '.join(self.fishes)}\nTotal animals: {Zoo.__animals}"
+        elif species == "bird":
+            return f"Birds in {self.name}: {', '.join(self.birds)}\nTotal animals: {Zoo.__animals}"
+
+
+zoo = Zoo(input())
+count = int(input())
+for _ in range(count):
+    species, name = input().split()
+    zoo.add_animal(species, name)
+
+name = input()
+print(zoo.get_info(name))
