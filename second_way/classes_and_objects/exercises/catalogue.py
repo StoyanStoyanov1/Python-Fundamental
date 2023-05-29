b7e02cc8d94d78debbe86afcb5cf7197a0ab17c2
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-05-29 07:04:45 +0200

    catalogue

---
+++ second_way/classes_and_objects/exercises/catalogue.py
@@ -0,0 +1,25 @@
+class Catalogue:
+
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    def add_product(self, product_name: str):
+        self.products.append(product_name)
+
+    def get_by_letter(self, first_letter: str):
+        return [item for item in self.products if item.startswith(first_letter)]
+
+    def __repr__(self):
+        return f'Items in the {self.name} catalogue:\n' + "\n".join(sorted(self.products))
+
+catalogue = Catalogue("Furniture")
+catalogue.add_product("Sofa")
+catalogue.add_product("Mirror")
+catalogue.add_product("Desk")
+catalogue.add_product("Chair")
+catalogue.add_product("Carpet")
+print(catalogue.get_by_letter("C"))
+print(catalogue)
+
+
