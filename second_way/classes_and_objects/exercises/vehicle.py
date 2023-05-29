aee1cb349e14bbd1df14c1636149a9d1cf7b87be
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-05-29 07:05:21 +0200

    vechicle

---
+++ second_way/classes_and_objects/exercises/vehicle.py
@@ -0,0 +1,36 @@
+class Vehicle:
+    def __init__(self, type, model, price):
+        self.type = type
+        self.model = model
+        self.price = price
+        self.owner = None
+
+    def buy(self, money: int, owner: str):
+        if money >= self.price and self.owner is None:
+            self.owner = owner
+            return f"Successfully bought a {self.type}. Change: {money - self.price:.2f}"
+        elif money < self.price:
+            return "Sorry, not enough money"
+        else:
+            return "Car already sold"
+
+    def sell(self):
+        if self.owner != None:
+            self.owner = None
+        return "Vehicle has no owner"
+
+    def __repr__(self):
+        if self.owner:
+            return f"{self.model} {self.type} is owned by: {self.owner}"
+        return f"{self.model} {self.type} is on sale: {self.price}"
+
+vehicle_type = "car"
+model = "BMW"
+price = 30000
+vehicle = Vehicle(vehicle_type, model, price)
+print(vehicle.buy(15000, "Peter"))
+print(vehicle.buy(35000, "George"))
+print(vehicle)
+vehicle.sell()
+print(vehicle)
+
